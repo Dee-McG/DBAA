4d76551ab158f85254833854f8473fbe48fec863
Datonex <cgomwe@ymail.com>
2022-06-17 22:15:30 +0100

    Fix url path to allow users to view other profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserProfileView
 
 urlpatterns = [
-    path('', UserProfileView.as_view(), name="profile"),
+    path('<int:user_id>/', UserProfileView.as_view(), name="profile"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -10,9 +10,10 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     template_name = 'profiles/profile.html'
     model = UserProfile
 
-    def get(self, request):
-        profile = get_object_or_404(self.model, user=request.user.id)
+    def get(self, request, user_id):
+        profile_name = get_object_or_404(self.model, user=user_id)
 
-        context = {"profile": profile}
+        context = {'profile_name': profile_name,
+                   'user_id': user_id}
 
         return render(request, self.template_name, context)
