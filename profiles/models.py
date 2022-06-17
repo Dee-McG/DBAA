83ee4fb8f9dba6684260bd3fb269847a3b79d036
Datonex <cgomwe@ymail.com>
2022-06-17 22:17:04 +0100

    Add signal to create profile when new user is made

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -10,3 +12,10 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create user profile"""
+    if created:
+        UserProfile.objects.create(user=instance)
