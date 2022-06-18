ddd430da57049c7f6146bf01c6fdabd2ae349567
hijazi-alaa <alaa.higazy@hotmail.com>
2022-06-18 15:24:39 +0200

    Added delete and edit events urls

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CreateEventView, EventList, EventDetailView
-
+from .views import ( CreateEventView,
+EventList, EventDetailView,
+DeleteEventView, EditEventView
+)
 urlpatterns = [
     path('create/', CreateEventView.as_view(), name="create_event"),
     path('events/', EventList.as_view(), name='events'),
     path('view/<slug:pk>/', EventDetailView.as_view(), name="view_event"),
+    path('edit/<slug:pk>/', EditEventView.as_view(), name="edit_event"),
+    path('delete/<slug:pk>/', DeleteEventView.as_view(), name="delete_event"),
 ]
