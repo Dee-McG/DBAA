d31cc29710a73f256f445f77465d713fc8e44aad
Datonex <cgomwe@ymail.com>
2022-06-18 19:05:36 +0100

    fix merge conflict

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+
 from django.views.generic import DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
