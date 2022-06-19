966a4bb19937d6b0fc931438c75e01ba93eed0a1
Datonex <cgomwe@ymail.com>
2022-06-19 17:56:08 +0100

    Add active button functionality to profile buttons

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from django.views.generic import DetailView, UpdateView, ListView
+from django.views.generic import DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import UserProfile
@@ -14,7 +14,7 @@ from events.models import EventNumbers
 class UserProfileView(LoginRequiredMixin, DetailView):
     """View to show user profile"""
 
-    template_name = 'profiles/profile.html'
+    template_name = "profiles/profile.html"
     model = UserProfile
 
     def post(self, request, pk):
@@ -32,7 +32,7 @@ class UserProfileView(LoginRequiredMixin, DetailView):
                 following=follower
             )
 
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
     def get(self, request, pk):
         # logged in user
@@ -53,14 +53,17 @@ class UserProfileView(LoginRequiredMixin, DetailView):
         following = Follow.objects.filter(
             user=user, following=profile_user)
 
+        profile_url = f"/profile/{pk}/"
+
         context = {
-            'user_id': pk,
-            'user': user,
-            'user_str': str(user),
-            'following': following,
-            'users_following': follow_lst,
-            'user_profile': user_profile,
-            'user_profile_str': str(user_profile),
+            "user_id": pk,
+            "user": user,
+            "user_str": str(user),
+            "following": following,
+            "users_following": follow_lst,
+            "user_profile": user_profile,
+            "user_profile_str": str(user_profile),
+            "profile_url": profile_url
         }
 
         return render(request, self.template_name, context)
@@ -69,13 +72,16 @@ class UserProfileView(LoginRequiredMixin, DetailView):
 class UserDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     """View to show user profile"""
 
-    template_name = 'profiles/personal_details.html'
+    template_name = "profiles/personal_details.html"
     model = UserProfile
 
     def get(self, request, pk):
         user_profile = get_object_or_404(self.model, user=self.request.user)
+        details_url = f"/profile/details/{pk}/"
+
         context = {
-            'user_profile': user_profile,
+            "user_profile": user_profile,
+            "details_url": details_url,
         }
 
         return render(request, self.template_name, context)
@@ -90,7 +96,7 @@ class UserEventView(LoginRequiredMixin, DetailView):
     event's friends of other user's are attending
     """
 
-    template_name = 'profiles/view_profile_events.html'
+    template_name = "profiles/view_profile_events.html"
     model = UserProfile
 
     def get(self, request, pk):
@@ -100,17 +106,18 @@ class UserEventView(LoginRequiredMixin, DetailView):
         user_profile = get_object_or_404(self.model, user=pk)
         # user object of who's profile you're viewing
         profile_user = get_object_or_404(User, id=pk)
-        # list of follow objects of users you're following
-        users_following = Follow.objects.filter(user=profile_user)
+
         events = EventNumbers.objects.filter(user=profile_user)
+        events_url = f"/profile/view-events/{pk}/"
 
         context = {
-            'user_id': pk,
-            'user': user,
-            'user_str': str(user),
-            'user_profile': user_profile,
-            'user_profile_str': str(user_profile),
-            'events': events
+            "user_id": pk,
+            "user": user,
+            "user_str": str(user),
+            "user_profile": user_profile,
+            "user_profile_str": str(user_profile),
+            "events": events,
+            "events_url": events_url
         }
 
         return render(request, self.template_name, context)
@@ -120,12 +127,12 @@ class EditAvatarView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """Update user avatar"""
 
     form_class = UserAvatarForm
-    template_name = 'profiles/edit_avatar.html'
+    template_name = "profiles/edit_avatar.html"
     model = UserProfile
 
     def form_valid(self, form):
         # if form is valid return profile
-        self.success_url = f'/profile/{self.request.user.id}/'
+        self.success_url = f"/profile/{self.request.user.id}/"
         return super().form_valid(form)
 
     def test_func(self):
@@ -133,10 +140,13 @@ class EditAvatarView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def get_context_data(self):
         user_profile = get_object_or_404(self.model, user=self.request.user)
+        pk = self.request.user.id
+        edit_avatar_url = f"/profile/edit-avatar/{pk}/"
 
         context = {
-            'user_profile': user_profile,
-            'form': UserAvatarForm(instance=user_profile)
+            "user_profile": user_profile,
+            "form": UserAvatarForm(instance=user_profile),
+            "edit_avatar_url": edit_avatar_url
         }
 
         return context
@@ -146,12 +156,12 @@ class EditDetailsView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """Update user avatar"""
 
     form_class = UserDetailForm
-    template_name = 'profiles/edit_details.html'
+    template_name = "profiles/edit_details.html"
     model = User
 
     def form_valid(self, form):
         # if form is valid return profile
-        self.success_url = f'/profile/details/{self.request.user.id}/'
+        self.success_url = f"/profile/details/{self.request.user.id}/"
         return super().form_valid(form)
 
     def test_func(self):
@@ -160,10 +170,13 @@ class EditDetailsView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def get_context_data(self):
         user_profile = get_object_or_404(UserProfile, id=self.request.user.id)
         data = get_object_or_404(self.model, id=self.request.user.id)
+        pk = self.request.user.id
+        edit_details_url = f"/profile/edit-details/{pk}/"
 
         context = {
-            'user_profile': user_profile,
-            'form': UserDetailForm(instance=data)
+            "user_profile": user_profile,
+            "form": UserDetailForm(instance=data),
+            "edit_details_url": edit_details_url
         }
 
         return context
